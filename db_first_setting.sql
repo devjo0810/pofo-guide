4be9dc9abbfba2915446683acddef1700083551c
devjo0810 <devjo0810@gmail.com>
2023-05-28 22:55:04 +0900

    db 초기 설정 변경

--- db_first_setting.sql
+++ db_first_setting.sql
@@ -1,5 +1,5 @@
 -- DB 생성
-CREATE DATABASE appmgmt
+CREATE DATABASE pofo
 CHARACTER SET = 'utf8'
 COLLATE = 'utf8_general_ci';
 
@@ -7,7 +7,7 @@ COLLATE = 'utf8_general_ci';
 CREATE USER 'pofo'@'%' IDENTIFIED BY 'pofo1234';
 
 -- 권한 추가
-GRANT ALL PRIVILEGES ON appmgmt.* TO 'pofo1234'@'%';
+GRANT ALL PRIVILEGES ON pofo.* TO 'pofo1234'@'%';
 
 -- 데이터베이스 변경
-USE DATABASE appmgmt;
+USE DATABASE pofo;
